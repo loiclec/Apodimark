823320f232ac52085c0170aac5644f513195a5d2
Logan Wright <logan.william.wright@gmail.com>
2016-11-09 19:15:08 -0500

    linux main

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,7 +4,7 @@ import XCTest
 @testable import ApodimarkTests
 @testable import PerformanceTests
 
-XCTMait([
+XCTMain([
     // ApodimarkTests
     testCase(CommonMarkConformanceTests.allTests),
     
